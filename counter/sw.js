ec511eba197b730b71d5d66cacae3dd782001847
williamhoza <williamhoza@gmail.com>
2021-10-28 17:13:02 -0700

    Update /counter/

--- counter/sw.js
+++ counter/sw.js
@@ -1,1 +1,38 @@
-console.log("Service worker created");
+console.log("Service worker created [3]");
+self.addEventListener('message', () => {
+  showNotification(null);
+});
+
+let dbReq = indexedDB.open("counter-db");
+let db = null;
+
+dbReq.onsuccess = function() {
+  db = dbReq.result;
+}
+
+self.addEventListener('notificationclick', (event) => {
+  if (event.action == 'rec') {
+    let transaction = db.transaction("events", "readwrite");
+    let eventsStore = transaction.objectStore("events");
+    
+    let newEvent = { timestamp: new Date() };
+    let req = eventsStore.add(newEvent);
+    req.onsuccess = function() {
+      showNotification(req.result);
+    }
+  }
+});
+
+function showNotification(total) {
+  
+  this.registration.showNotification("Counter", {
+    body: "Ready to log." + (total == null ? "" : " Total events logged: " + total),
+    actions: [
+      {
+        action: "rec",
+        title: "Log Event"
+      }
+    ],
+    tag: "singleton"
+  });
+}
