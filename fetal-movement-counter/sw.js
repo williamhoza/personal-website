bb80b9855d111e6567a7f2544636327f892732c6
williamhoza <williamhoza@gmail.com>
2021-10-30 16:38:21 -0700

    Fix bug when service worker is terminated (?)

--- fetal-movement-counter/sw.js
+++ fetal-movement-counter/sw.js
@@ -1,38 +1,63 @@
 console.log("Service worker created [3]");
 self.addEventListener('message', () => {
-  showNotification(null);
+  showNotification(true, null);
 });
 
-let dbReq = indexedDB.open("fetal-movement-counter-db");
-let db = null;
+let dbGetter;
 
-dbReq.onsuccess = function() {
-  db = dbReq.result;
+// Based on https://github.com/jakearchibald/svgomg/blob/main/src/js/utils/storage.js#L5
+function getDB() {
+  if (dbGetter) return dbGetter;
+  
+  dbGetter = new Promise((resolve, reject) => {
+    const req = indexedDB.open("fetal-movement-counter-db");
+    
+    req.onerror = () => {
+      reject(req.error);
+    };
+    
+    req.onsuccess = () => {
+      resolve(req.result);
+    };
+  });
+  
+  return dbGetter;
 }
 
 self.addEventListener('notificationclick', (event) => {
   if (event.action == 'rec') {
-    let transaction = db.transaction("fetal-movements", "readwrite");
-    let eventsStore = transaction.objectStore("fetal-movements");
-    
-    let newEvent = { timestamp: new Date() };
-    let req = eventsStore.add(newEvent);
-    req.onsuccess = function() {
-      showNotification(req.result);
-    }
+    showNotification(false, null);
+    getDB().then(db => {
+      let transaction = db.transaction("fetal-movements", "readwrite");
+      let eventsStore = transaction.objectStore("fetal-movements");
+      
+      let newEvent = { timestamp: new Date() };
+      let req = eventsStore.add(newEvent);
+      req.onsuccess = function() {
+        showNotification(true, req.result);
+      }
+    });
   }
 });
 
-function showNotification(total) {
-  
-  this.registration.showNotification("Fetal Movement Counter", {
-    body: "Ready!" + (total == null ? "" : " Total movements logged: " + total),
-    actions: [
+function showNotification(ready, total) {
+  let nBody, nActions;
+  if (ready) {
+    nBody = "Ready!" + (total == null ? "" : " Total movements logged: " + total);
+    nActions = [
       {
         action: "rec",
         title: "Log Movement"
       }
-    ],
+    ];
+  } else {
+    nBody = "Working...";
+    nActions = [];
+  }
+  
+  this.registration.showNotification("Fetal Movement Counter", {
+    body: nBody,
+    actions: nActions,
     tag: "singleton",
     icon: "pregnancy.png",
     badge: "pregnancy.png"
